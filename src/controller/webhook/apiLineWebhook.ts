9e4fddb05257de2d1834b3146d33b7466b42fa74
JeffAiii <jeff@aiii.ai>
2025-09-02 18:01:26 +0800

    style: 調整引入語句的格式以符合風格指南

--- src/controller/webhook/apiLineWebhook.ts
+++ src/controller/webhook/apiLineWebhook.ts
@@ -1,5 +1,5 @@
-import { LineWebhook } from "@chihhaocooly/chihhao-package";
-import { Request, Response } from "express";
+import { LineWebhook } from '@chihhaocooly/chihhao-package';
+import { Request, Response } from 'express';
 
 let _lineWebhook: LineWebhook | null = null;
 let _initPromise: Promise<void> | null = null;
@@ -8,18 +8,18 @@ let _initPromise: Promise<void> | null = null;
  * 獲取唯一實例，保證多個請求同時進入時不會重複執行 init()
  */
 async function getInstance(): Promise<LineWebhook> {
-    if (!_lineWebhook) {
-        if (!_initPromise) {
-            // 如果沒有初始化，就建立初始化 Promise
-            _initPromise = (async () => {
-                _lineWebhook = new LineWebhook();
-                await _lineWebhook.init();
-            })();
-        }
-        // 等待初始化完成
-        await _initPromise;
+  if (!_lineWebhook) {
+    if (!_initPromise) {
+      // 如果沒有初始化，就建立初始化 Promise
+      _initPromise = (async () => {
+        _lineWebhook = new LineWebhook();
+        await _lineWebhook.init();
+      })();
     }
-    return _lineWebhook!;
+    // 等待初始化完成
+    await _initPromise;
+  }
+  return _lineWebhook!;
 }
 
 /**
@@ -28,7 +28,9 @@ async function getInstance(): Promise<LineWebhook> {
  * @param res
  */
 export const apiLineWebhook = async (req: Request, res: Response) => {
-    console.log('apiLineWebhook');
-    const lineWebhook = await getInstance();
-    return lineWebhook.lineWebhookOnRequest(req, res);
+  console.log('apiLineWebhook');
+  console.log('🚀 headers =>', JSON.stringify(req.headers));
+  console.log('🚀 path =>', req.path, ' body =>', JSON.stringify(req.body));
+  const lineWebhook = await getInstance();
+  return lineWebhook.lineWebhookOnRequest(req, res);
 };
